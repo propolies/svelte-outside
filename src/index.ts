5d583550afabf66fd0ff9f43acdf226c0d44ab45
propolies <propolies@gmail.com>
2024-04-24 18:10:43 +0200

    fix use event instead of node

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-export function clickOutside(node: Node, callback: (node: Node) => void) {
+export function clickOutside(node: Node, callback: (node: Event) => void) {
   return outside(node, "click", callback)
 }
 
-export function tapOutside(node: Node, callback: (node: Node) => void) {
+export function tapOutside(node: Node, callback: (node: Event) => void) {
   return outside(node, "mousedown", callback)
 }
 
-function outside(node: Node, listener: string, callback: (node: Node) => void) {
+function outside(node: Node, listener: string, callback: (node: Event) => void) {
   
   const handleClick = (event: Event) => {
     if (node && !node.contains(event.target as Node) && !event.defaultPrevented) {
-      callback(node)
+      callback(event)
     }
   }
 
